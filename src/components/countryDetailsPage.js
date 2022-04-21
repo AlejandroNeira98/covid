b3d6be75a6ca846274746a955eb1fae70c25fce3
AlejandroNeira98 <ja.neira@uniandes.edu.co>
2022-04-21 13:16:07 -0500

    Display Graphs

--- src/components/countryDetailsPage.js
+++ src/components/countryDetailsPage.js
@@ -1,54 +1,96 @@
 import React, { useEffect } from 'react';
 import { shallowEqual, useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import { Line } from 'react-chartjs-2';
+import {
+  Chart, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend,
+} from 'chart.js';
 import { getCountry } from '../redux/CountryDetails/countryReducer';
 
 export default function CountryDetails() {
   const dispatch = useDispatch();
   const { id, name } = useParams();
 
-  useEffect(() => dispatch(getCountry(id, name)), []);
+  useEffect(() => { dispatch(getCountry(id, name)); }, []);
+
+  Chart.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
+  );
 
   const {
-    dates, recovered, confirmed, hospitalized, deaths,
+    confirmed, deaths, dates, dateStart, dateEnd, thisDay,
   } = useSelector((state) => state.country, shallowEqual);
 
   const handleDateChange = (start, end) => {
     dispatch(getCountry(id, name, start, end));
   };
-  console.log( dates, recovered, confirmed, hospitalized, deaths);
-  return (
-    <form>
-      <label htmlFor="start">
-        Start date:
-        <input
-          type="date"
-          id="start"
-          /* value={ yesterday } */
-          min="2018-01-01"
-          max="2018-12-31"
-        />
-      </label>
-      <label htmlFor="end">
-        Start date:
-        <input
-          type="date"
-          id="end"
-          /* value={ today } */
-          min="2018-01-01"
-          max="2018-12-31"
-        />
-      </label>
+  console.log( dateStart, dateEnd, thisDay);
 
-      <label htmlFor="category">
-        Choose a category:
-        <select name="category" id="category">
-          <option value="confirmed">Confirmed</option>
-          <option value="recovered">Recovered</option>
-          <option value="hospitalized_patients">hospitalized patients</option>
-          <option value="deaths">Deaths</option>
-        </select>
-      </label>
-    </form>
+  const data = (x, y, category) => ({
+    x,
+    datasets: [
+      {
+        label: `${category}`,
+        data: y,
+        borderColor: 'rgb(255, 99, 132)',
+        backgroundColor: 'rgba(255, 99, 132, 0.5)',
+      },
+    ],
+  });
+
+  const lineOptions = () => ({
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top',
+      },
+      title: {
+        display: true,
+        text: `${name}:`,
+      },
+    },
+  });
+
+  return (
+    <div>
+      <form>
+        <label htmlFor="start">
+          Start date:
+          <input
+            type="date"
+            id="start"
+            value={dateStart && `${dateStart.yyyy}-${dateStart.mm}-${dateStart.dd}`}
+            min="2018-01-01"
+            max={thisDay && `${thisDay.yyyy}-${thisDay.mm}-${thisDay.dd}`}
+          />
+        </label>
+        <label htmlFor="end">
+          End date:
+          <input
+            type="date"
+            id="end"
+            value={dateEnd && `${dateEnd.yyyy}-${dateEnd.mm}-${dateEnd.dd}`}
+            min="2018-01-01"
+            max={thisDay && `${thisDay.yyyy}-${thisDay.mm}-${thisDay.dd}`}
+          />
+        </label>
+
+        <label htmlFor="category">
+          Choose a category:
+          <select name="category" id="category">
+            <option value="confirmed">Confirmed</option>
+            <option value="deaths">Deaths</option>
+          </select>
+        </label>
+      </form>
+      <Line options={lineOptions()} data={data(dates, deaths, 'New Deaths')} />
+      <Line options={lineOptions()} data={data(dates, confirmed, 'New Confirmed')} />
+    </div>
   );
 }
--- src/redux/CountryDetails/countryReducer.js
+++ src/redux/CountryDetails/countryReducer.js
@@ -2,57 +2,79 @@ const url = 'https://api.covid19tracking.narrativa.com/api/country/';
 
 const FETCH_COUNTRY_DATA = 'covid-stats/Home/FETCH_COUNTRY_DATA';
 
-function today() {
-  let today = new Date();
-  const dd = String(today.getDate()).padStart(2, '0');
-  const mm = String(today.getMonth() + 1).padStart(2, '0');
-  const yyyy = today.getFullYear();
-  today = `${yyyy}-${mm}-${dd}`;
-  return today;
-}
-
-function aWeekAgo() {
-  const today = new Date();
-  let aWeekAgo = new Date(today.getTime() - 7 * 24 * 60 * 60 * 1000);
-  const dd = String(aWeekAgo.getDate()).padStart(2, '0');
-  const mm = String(aWeekAgo.getMonth() + 1).padStart(2, '0');
-  const yyyy = aWeekAgo.getFullYear();
-  aWeekAgo = `${yyyy}-${mm}-${dd}`;
-  return aWeekAgo;
-}
-
 export default function countryReducer(state = {}, action) {
   let datesInfo = [];
-  const dates = [];
   const recovered = [];
   const confirmed = [];
-  const hospitalized = [];
   const deaths = [];
+  const dates = [];
 
   switch (action.type) {
     case FETCH_COUNTRY_DATA:
       datesInfo = Object.entries(action.data.dates);
       datesInfo.forEach((date) => {
-        dates.push(date[0]);
-        recovered.push(date[1].countries[action.name].today_new_recovered);
-        confirmed.push(date[1].countries[action.name].today_new_confirmed);
-        hospitalized.push(date[1].countries[action.name].today_new_total_hospitalised_patients);
-        deaths.push(date[1].countries[action.name].today_new_deaths);
+        recovered.push(
+          [
+            date[1].countries[action.name].today_new_recovered,
+          ],
+        );
+        confirmed.push(
+          [date[0],
+            date[1].countries[action.name].today_new_confirmed,
+          ],
+        );
+        deaths.push(
+          [date[0],
+            date[1].countries[action.name].today_new_deaths,
+          ],
+        );
+        dates.push(
+          [date[0]],
+        );
       });
       return {
-        dates, recovered, confirmed, hospitalized, deaths,
+        recovered,
+        confirmed,
+        deaths,
+        dates,
+        dateStart: action.dateStart,
+        dateEnd: action.dateEnd,
+        thisDay: action.thisDay,
       };
     default:
       return state;
   }
 }
 
-export function getCountry(countryId, name, dateStart = aWeekAgo(), dateEnd = today()) {
+function today() {
+  let today = new Date();
+  const dd = String(today.getDate()).padStart(2, '0');
+  const mm = String(today.getMonth() + 1).padStart(2, '0');
+  const yyyy = today.getFullYear();
+  today = { yyyy, mm, dd };
+  return today;
+}
+
+function aWeekAgo() {
+  const today = new Date();
+  let aWeekAgo = new Date(today.getTime() - 20 * 24 * 60 * 60 * 1000);
+  const dd = String(aWeekAgo.getDate()).padStart(2, '0');
+  const mm = String(aWeekAgo.getMonth() + 1).padStart(2, '0');
+  const yyyy = aWeekAgo.getFullYear();
+  aWeekAgo = { yyyy, mm, dd };
+  return aWeekAgo;
+}
+
+export function getCountry(
+  countryId, name, dateStart = aWeekAgo(), dateEnd = today(), thisDay = today(),
+) {
   return async (dispatch) => {
-    await fetch(`${url}${countryId}?date_from=${dateStart}&date_to=${dateEnd}`)
+    await fetch(`${url}${countryId}?date_from=${dateStart.yyyy}-${dateStart.mm}-${dateStart.dd}&date_to=${dateEnd.yyyy}-${dateEnd.mm}-${dateEnd.dd}`)
       .then((response) => response.json())
       .then((data) => {
-        dispatch({ type: FETCH_COUNTRY_DATA, data, name });
+        dispatch({
+          type: FETCH_COUNTRY_DATA, data, name, dateStart, dateEnd, thisDay,
+        });
       });
   };
 }
