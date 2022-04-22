6fe98ce7e7f7dcfb9f8ca9b385c063a71fbbe7ab
AlejandroNeira98 <ja.neira@uniandes.edu.co>
2022-04-22 16:30:56 -0500

    Implement search bar

--- src/components/countries.js
+++ src/components/countries.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { shallowEqual, useSelector } from 'react-redux';
 import CountryCard from './countryCard';
 
 export default function Countries() {
   const countries = useSelector((state) => state.countries, shallowEqual);
   const countriesList = Object.entries(countries);
+  const [searchValue, setSearchValue] = useState('');
+  const handleSearch = (e) => {
+    setSearchValue(e.target.value)
+  }
 
   return (
+  <>
+    <div>
+      <input type='text' onChange={handleSearch} value={searchValue} placeholder='Search'></input>
+    </div>
     <div style={{ display: 'flex', flexWrap: 'wrap' }}>
-      {countriesList.map((country) => (
+      {countriesList.filter((country) => {
+        if (searchValue === ''){
+          return country
+        } else if (country[0].toLowerCase().includes(searchValue.toLowerCase())) {
+          return country
+        }
+      }).map((country) => (
         <CountryCard
           name={country[0]}
           deaths={country[1].today_new_deaths}
@@ -19,5 +33,6 @@ export default function Countries() {
         />
       ))}
     </div>
+  </>
   );
 }
